ef0f9696d5a51ebbe705d1d557012f3a952c8d1b
AIdevel <sanjayshaw80138@gmail.com>
2025-03-11 13:29:14 +0530

    "1.Added the Language Change Functionalities in App  2.Added the Differnt screen size support"

--- README.md
+++ README.md
@@ -1,5 +1,58 @@
-# web_portfolio
+# Portfolio
 
 ``` 
 flutter pub get
+```
+** Add Difference Language in App
+```
+flutter pub add flutter_localizations --sdk=flutter
+```
+```
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+```
+* Inside Material Widget in Main
+ ```
+ localizationsDelegates: [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      ```
+* Add SupportedLocales
+```
+   supportedLocales: [
+        Locale('en'), // English
+        Locale('es'), // Spanish
+      ],
+```
+* Add intl
+```
+  flutter pub add intl:any
+```
+
+* Add yaml
+```
+flutter:
+  generate: true # Add this line
+```
+*Create l10n folder inside lib
+**Create arb file of all Language
+```
+app_en.arb
+app_fa.arb
+
+```
+
+*Add a new yaml file to the root directory of the Flutter project. Name this file l10n.yaml
+
+```
+arb-dir: lib/l10n
+template-arb-file: app_en.arb
+output-localization-file: app_localizations.dart
+```
+* Use 
+```
+AppLocalizations.of(context)!.home # it's must be changeable eg home
 ```
---
+++ lib/screen/Home/my_appbar.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:web_portfolio/common/extension.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+class MyAppbar extends StatelessWidget {
+  const MyAppbar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        AppLogo(),
+        Spacer(),
+        AppMenus(),
+        Spacer(),
+        LanguageToggle(),
+        ThemeToggle(),
+      ],
+    );
+  }
+}
+
+class AppLogo extends StatelessWidget {
+  const AppLogo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('Portfolio', style: context.textStyle.titleLgBold);
+  }
+}
+
+class AppMenus extends StatelessWidget {
+  const AppMenus({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Text(AppLocalizations.of(context)!.home),
+        Text(' Skills and Expertise'),
+        Text('Project'),
+        Text('Education and Certifications'),
+        Text('About Me'),
+      ],
+    );
+  }
+}
+
+class LanguageToggle extends StatelessWidget {
+  const LanguageToggle({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return PopupMenuButton(
+      itemBuilder: (context) {
+        return [
+          PopupMenuItem(child: Text('English')),
+          PopupMenuItem(child: Text('Hindi')),
+          PopupMenuItem(child: Text('Spanish')),
+        ];
+      },
+    );
+  }
+}
+
+class ThemeToggle extends StatelessWidget {
+  const ThemeToggle({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Switch(value: false, onChanged: (value) {});
+  }
+}
